39b344b09b0529def0aa9e3ebe5a00bd00f67f8d
Aayush Kapar <aayushkapar@gmail.com>
2020-03-16 00:43:30 -0400

    update script

--- timedata.sh
+++ timedata.sh
@@ -1,3 +1,23 @@
-csvtojson csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv > csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-ConfirmedT.json
-csvtojson csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv > csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-DeathT.json
-csvtojson csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv > csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-RecoveredT.json
+commit_message="$1"
+
+echo pull remote git 
+git merge upstream/master
+
+echo remove old json files
+rm csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.json
+rm csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.json
+rm csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.json
+
+echo make new json files
+csvtojson csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv > csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.json
+csvtojson csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv > csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.json
+csvtojson csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv > csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.json
+
+
+echo add json to git
+git add csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.json
+git add csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.json
+git add csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.json
+
+git commit -m "$commit_message"
+git push
