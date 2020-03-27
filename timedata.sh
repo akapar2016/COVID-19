88400f29a8f8d33df3201899f5f097165ffe4742
Aayush Kapar <aayushkapar@gmail.com>
2020-03-26 21:59:08 -0400

    Scrip change for database name change

--- timedata.sh
+++ timedata.sh
@@ -1,6 +1,7 @@
 commit_message="$1"
 
 echo pull remote git 
+git pull
 git fetch upstream
 git checkout master
 git merge upstream/master
@@ -11,9 +12,9 @@ rm csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.json
 rm csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.json
 
 echo make new json files
-csvtojson csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv > csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.json
-csvtojson csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv > csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.json
-csvtojson csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv > csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.json
+csvtojson csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv > csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.json
+csvtojson csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv > csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.json
+csvtojson csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv > csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.json
 
 
 echo add json to git
